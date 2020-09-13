83b05c701285106ea1e71bed464cea42dd0e8410
GeorgeSpark-Stahl <mr.marmot@gmail.com>
2020-09-13 23:32:06 +0000

    Add Example Sudoku Display

--- src/App.tsx
+++ src/App.tsx
@@ -4,22 +4,27 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <table>
+        <caption>Sudoku</caption>
+        <colgroup><col/><col/><col/></colgroup>
+        <colgroup><col/><col/><col/></colgroup>
+        <colgroup><col/><col/><col/></colgroup>
+        <tbody>
+        <tr><td>1</td><td> </td><td>3</td><td>6</td><td></td><td>4</td><td>7</td><td></td><td>9</td></tr>
+        <tr><td></td><td>2</td><td> </td><td></td><td>9</td><td></td><td> </td><td>1</td><td></td></tr>
+        <tr><td>7</td><td> </td><td></td><td></td><td></td><td></td><td></td><td></td><td>6</td></tr>
+        </tbody>
+        <tbody>
+        <tr><td>2</td><td></td><td>4</td><td></td><td>3</td><td></td><td>9</td><td></td><td>8</td></tr>
+        <tr><td></td><td></td><td></td><td></td><td> </td><td></td><td></td><td></td><td></td></tr>
+        <tr><td>5</td><td> </td><td> </td><td>9 </td><td> </td><td>7</td><td> </td><td> </td><td>1</td></tr>
+        </tbody>
+        <tbody>
+        <tr><td>6</td><td> </td><td> </td><td> </td><td>5</td><td> </td><td> </td><td> </td><td>2</td></tr>
+        <tr><td></td><td></td><td></td><td></td><td>7</td><td> </td><td> </td><td> </td><td></td></tr>
+        <tr><td>9</td><td>  </td><td>  </td><td>8 </td><td> </td><td>2 </td><td>  </td><td> </td><td>5</td></tr>
+        </tbody>
+  </table>
   );
 }
 
