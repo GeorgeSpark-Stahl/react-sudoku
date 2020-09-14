dc70ee0ae5ab73303f51958698b8ec534fe441b4
GeorgeSpark-Stahl <mr.marmot@gmail.com>
2020-09-14 02:16:08 +0000

    Add Input Box

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,32 @@ import './App.css';
 import { utils } from './utils';
 
 function App() {
+  const [table, setTable] = useState([[1,0,0,0,0,0,0,0,0],
+                                      [2,0,0,0,0,0,0,0,0],
+                                      [3,0,0,0,0,0,0,0,0],
+                                      [4,0,0,0,0,0,0,0,0],
+                                      [5,0,0,0,0,0,0,0,0],
+                                      [6,0,0,0,0,0,0,0,0],
+                                      [7,0,0,0,0,0,0,0,0],
+                                      [8,0,0,0,0,0,0,0,0],
+                                      [9,0,0,0,0,0,0,0,0]]);
   return (
     <table>
         <caption>Sudoku</caption>
         <colgroup><col/><col/><col/></colgroup>
         <colgroup><col/><col/><col/></colgroup>
         <colgroup><col/><col/><col/></colgroup>
-        {utils.range(1, 3).map(groupId => (
+        {utils.range(0, 2).map(groupId => (
           <tbody key={groupId}>
-            {utils.range(1, 3).map(rowId => (
+            {utils.range(0, 2).map(rowId => (
               <tr key={rowId}>
-                {utils.range(1, 9).map(columnId => (
+                {utils.range(0, 8).map(columnId => (
                   <td key={columnId}>
-                    {columnId}
+                    <input 
+                      value={table[groupId*3+rowId][columnId]}
+                      readOnly={true}
+                      size={1} >
+                    </input>
                   </td>
                 ))}
               </tr>
